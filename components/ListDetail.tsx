e4ac09eaf5d157b45085e0385dc072c59bf079b9
seri1234 <abc_x_y_z_12@yahoo.co.jp>
2023-08-10 15:57:10 +0900

    jsonから情報を正しく取得

--- components/ListDetail.tsx
+++ components/ListDetail.tsx
@@ -11,7 +11,7 @@ const ListDetail = ({ item: post }: ListDetailProps) => (
     <h1>タイトル {post.title}</h1>
     <p>ID: {post.id}</p>
     <p>ID: {post.title}</p>
-    <p>ID: {post.content}</p>
+    <p>ID: {post.contentHtml}</p>
     <p>ID: {post.createdAt}</p>
     <p>ID: {post.updatedAt}</p>
   </div>
